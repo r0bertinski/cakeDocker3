d2ef0280682b6abd71034218f9bd0404227c2dd8
Roberto Schuh <robbyschuh@gmail.com>
2021-07-21 09:38:45 +0700

    added remove script && update to create db if no exists command

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/bin/mysqld_safe --skip-grant-tables &
 sleep 5
-# mysql -u root -e "CREATE DATABASE cms"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS cms"
 mysql -u root cms < /tmp/db.sql
